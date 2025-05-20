84a3fd2aa5a1e091050e7e1e41fe1918cdd5fb8d
JarviS6051 <yashb150603@gmail.com>
2025-05-20 14:30:30 +0530

    Done

--- src/index.js
+++ src/index.js
@@ -7,6 +7,10 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.get("/", (req, res) => {
+  res.send("Server is up and running!");
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port: ${PORT} and connected to Mongo URI: ${process.env.MONGO_URI}`);
 });
