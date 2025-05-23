9d7848e1307aa7ef99491302cad006c90329694b
JarviS6051 <yashb150603@gmail.com>
2025-05-23 18:29:25 +0530

    Done for today

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
   res.send("Server is up and running!");
